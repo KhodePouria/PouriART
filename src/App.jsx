db4e5b9ef132c051d5a3d65972d7a99b218e727e
Pouria <pooriaramezani1382@gmail.com>
2025-04-29 16:10:33 +0330

    add vercel analythics

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import MyPic from './MyPicture.jsx'
+import { Analytics } from '@vercel/analytics/react'
 import Mountains from './Mountains.jsx'
 
 function App() {
